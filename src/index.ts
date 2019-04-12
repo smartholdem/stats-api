8ff6277a9baf45b289663c47bcaa81d6af5fd464
technolog <viktor@allz.su>
2019-04-12 23:36:53 +0300

    index

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,23 @@
 import * as http from 'http';
-import * as debug from 'debug';
+import config from './config/config';
+import { Socket } from './socket';
 
-import App from './App';
+const app = require('./config/express').default();
 
-debug('ts-express:server');
+const server: http.Server = new http.Server(app);
 
+new Socket(server);
 
-// -->Set: port
-const port = normalizePort(3000);
-App.set('port', port);
+server.listen(config.port);
 
-// -->Set: headers
-App.all('*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    next();
+server.on('error', (e: Error) => {
+  console.log('Error starting server' + e);
 });
 
-
-
-const server = http.createServer(App);
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-function normalizePort(val: number|string): number|string|boolean {
-    let port: number = (typeof val === 'string') ? parseInt(val, 10) : val;
-    if (isNaN(port)) return val;
-    else if (port >= 0) return port;
-    else return false;
-}
-
-function onError(error: NodeJS.ErrnoException): void {
-    if (error.syscall !== 'listen') throw error;
-    let bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
-    switch(error.code) {
-        case 'EACCES':
-            console.error(`${bind} requires elevated privileges`);
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(`${bind} is already in use`);
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-
-function onListening(): void {
-    let addr = server.address();
-    let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
-    debug(`Listening on ${bind}`);
-}
+server.on('listening', () => {
+  if (config.useMongo) {
+    console.log(`Server started on port ${config.port} on env ${process.env.NODE_ENV || 'dev'} dbcon ${config.mongodb}`);
+  } else {
+    console.log(`Server started on port ${config.port} on env ${process.env.NODE_ENV || 'dev'}`);
+  }
+});
