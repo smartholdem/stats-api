27d01c686bcf647e3afb28292f74a651bbd10847
technolog <viktor@allz.su>
2019-04-13 15:19:17 +0300

    syncInit

--- src/controllers/index.server.controller.ts
+++ src/controllers/index.server.controller.ts
@@ -1,12 +1,51 @@
 import {Request, Response} from 'express';
 import * as smartholdemApi from 'sthjs-wrapper';
 import * as level from 'level';
+import * as scheduler from 'node-schedule';
 
 const db = level('.db', {valueEncoding: 'json'});
 
 /**  SmartHoldem API init **/
 smartholdemApi.setPreferredNode("192.168.1.55");
 smartholdemApi.init("main"); //main or dev
+
+// 0x0 - total tx count
+// 0x1 - uniq addresses
+// 0x100 - tx count by day
+// 0x200 - amount transfer by day
+// 0x300 - price by day
+
+let timeStart = 1511269200;
+let dayKey = '20171121';
+
+let options = {
+    txOffset: 0,
+    txLimit: 50,
+    txDay: 0,
+    amountDay: 0,
+    addresses: 0
+};
+
+let parameters = {
+    "limit": options.txLimit,
+    "offset": options.txOffset,
+    "orderBy": "height:asc"
+};
+
+
+// async function always returns a Promise
+async function syncInit(): Promise<void> {
+    await scheduler.scheduleJob("*/5 * * * * *", () => {
+
+    });
+}
+
+smartholdemApi.getTransactionsList(parameters, (error, success, response) => {
+    if (response.success) {
+
+    }
+    console.log(response.transactions);
+});
 
 export default class IndexController {
     public index(req: Request, res: Response, next: Function): void {
@@ -34,9 +73,22 @@ export default class IndexController {
             "offset": 0
         };
         smartholdemApi.getTransactionsList(parameters, (error, success, response) => {
-            res.json(response.count);
+            res.json({count: response.count});
+        });
+    }
+
+    public getTxList(req: Request, res: Response): void {
+        let parameters = {
+            "limit": 2,
+            "offset": 0,
+            "orderBy": "height:asc"
+        };
+        smartholdemApi.getTransactionsList(parameters, (error, success, response) => {
+            res.json(response);
         });
     }
 }
+
+syncInit();
 
 export const indexController = new IndexController();
